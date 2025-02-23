5f0e22b2a4af050e3469ea2cf414de3500e55583
Cortezinho <gabrielcortez2802@gmail.com>
2025-02-23 14:22:35 -0300

    Alteração na Atividade2 IFACI

--- IFACI/IFACI/C#/aula02/atividade_2/objetivo_3/Form1.Designer.cs
+++ IFACI/IFACI/C#/aula02/atividade_2/objetivo_3/Form1.Designer.cs
@@ -39,18 +39,20 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.pictureBox1.Location = new System.Drawing.Point(294, 25);
+            this.pictureBox1.Location = new System.Drawing.Point(401, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(182, 167);
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(281, 237);
+            this.textBox1.Location = new System.Drawing.Point(390, 303);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 41);
+            this.textBox1.Size = new System.Drawing.Size(273, 50);
             this.textBox1.TabIndex = 1;
             this.textBox1.TabStop = false;
             // 
@@ -58,9 +60,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Chartreuse;
             this.button1.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(46, 337);
+            this.button1.Location = new System.Drawing.Point(61, 415);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 72);
+            this.button1.Size = new System.Drawing.Size(233, 89);
             this.button1.TabIndex = 0;
             this.button1.Text = "&LIGA";
             this.button1.UseVisualStyleBackColor = false;
@@ -70,9 +73,10 @@
             // 
             this.button2.BackColor = System.Drawing.Color.OrangeRed;
             this.button2.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(294, 337);
+            this.button2.Location = new System.Drawing.Point(402, 415);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(182, 72);
+            this.button2.Size = new System.Drawing.Size(243, 89);
             this.button2.TabIndex = 1;
             this.button2.Text = "&DESLIGA";
             this.button2.UseVisualStyleBackColor = false;
@@ -82,9 +86,10 @@
             // 
             this.button3.BackColor = System.Drawing.Color.Yellow;
             this.button3.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(582, 337);
+            this.button3.Location = new System.Drawing.Point(776, 415);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 72);
+            this.button3.Size = new System.Drawing.Size(232, 89);
             this.button3.TabIndex = 2;
             this.button3.Text = "&SAIR";
             this.button3.UseVisualStyleBackColor = false;
@@ -92,14 +97,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.ClientSize = new System.Drawing.Size(1067, 534);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Aplicativo";
             this.Load += new System.EventHandler(this.Form1_Load);
