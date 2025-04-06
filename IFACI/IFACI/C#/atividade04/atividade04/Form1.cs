842b2b29a9d30e50f061d4d12df61b550857ef7c
Cortezinho <gabrielcortez2802@gmail.com>
2025-04-06 20:18:01 -0300

    atividade 4 IFACI

---
+++ IFACI/IFACI/C#/atividade04/atividade04/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade04
+{
+    public partial class Form1: Form
+    {
+        private List<Image> imagensTanque;
+        private List<Image> imagensNivel;
+        private int indiceAtualTanque = 0;
+        private int indiceAtualNivel = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            imagensTanque = new List<Image>
+            {
+                Image.FromFile("c:\\imagens\\tanque_umQuinto.png"),
+                Image.FromFile("c:\\imagens\\tanque_doisQuintos.png"),
+                Image.FromFile("c:\\imagens\\tanque_tresQuintos.png"),
+                Image.FromFile("c:\\imagens\\tanque_quatroQuintos.png"),
+                Image.FromFile("c:\\imagens\\tanque_cheio.png")
+            };
+
+            imagensNivel = new List<Image>
+            {
+                Image.FromFile("c:\\imagens\\Dig_1.bmp"),
+                Image.FromFile("c:\\imagens\\Dig_2.bmp"),
+                Image.FromFile("c:\\imagens\\Dig_3.bmp"),
+                Image.FromFile("c:\\imagens\\Dig_4.bmp"),
+                Image.FromFile("c:\\imagens\\Dig_5.bmp")
+            };
+        }
+
+        // botao sair
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult bc;
+            MessageBoxButtons bot = MessageBoxButtons.YesNo;
+            MessageBoxIcon icon = MessageBoxIcon.Error;
+            MessageBoxDefaultButton bd = MessageBoxDefaultButton.Button1;
+            bc = MessageBox.Show("Certeza que\ndeseja sair?", "Infomação", bot, icon, bd);
+
+            switch (bc)
+            {
+                case DialogResult.Yes:
+                    Application.Exit();
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToShortDateString();
+            label2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Process.Start("calc");
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad");
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("c:\\imagens\\tanque_vazio.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = Image.FromFile("c:\\imagens\\Dig_0.bmp");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        
+        //botao ligar
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+            button2.Enabled = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            indiceAtualTanque = (indiceAtualTanque + 1) % imagensTanque.Count;
+            pictureBox1.Image = imagensTanque[indiceAtualTanque];
+
+            indiceAtualNivel = (indiceAtualNivel + 1) % imagensNivel.Count;
+            pictureBox2.Image = imagensNivel[indiceAtualNivel];
+        }
+
+        //botao emergencia
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();           
+            button2.Enabled = true;  
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer2.Stop(); 
+
+            
+            indiceAtualTanque = 0;
+            indiceAtualNivel = 0;
+
+            
+            pictureBox1.Image = imagensTanque[indiceAtualTanque];
+            pictureBox2.Image = imagensNivel[indiceAtualNivel];
+
+            button2.Enabled = true; 
+        }
+    }
+}
--- IFACI/IFACI/C#/aula04/Exemplo_04/Form1.Designer.cs
+++ IFACI/IFACI/C#/aula04/Exemplo_04/Form1.Designer.cs
@@ -30,25 +30,25 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.button11 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,10 +56,11 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Location = new System.Drawing.Point(4, 4);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(393, 318);
+            this.tabControl1.Size = new System.Drawing.Size(524, 391);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -75,90 +76,21 @@
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.pictureBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(385, 292);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(516, 362);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Temperatura";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.button12);
-            this.tabPage2.Controls.Add(this.button11);
-            this.tabPage2.Controls.Add(this.pictureBox2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(385, 292);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Motor";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(169, 258);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(191, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 42);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "0";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(191, 67);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 42);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "1";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(191, 115);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 42);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "2";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(191, 163);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 42);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "3";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(191, 211);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(83, 42);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "4";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(280, 211);
+            this.button6.Location = new System.Drawing.Point(373, 260);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(83, 42);
+            this.button6.Size = new System.Drawing.Size(111, 52);
             this.button6.TabIndex = 10;
             this.button6.Text = "9";
             this.button6.UseVisualStyleBackColor = true;
@@ -166,9 +98,10 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(280, 163);
+            this.button7.Location = new System.Drawing.Point(373, 201);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(83, 42);
+            this.button7.Size = new System.Drawing.Size(111, 52);
             this.button7.TabIndex = 9;
             this.button7.Text = "8";
             this.button7.UseVisualStyleBackColor = true;
@@ -176,9 +109,10 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(280, 115);
+            this.button8.Location = new System.Drawing.Point(373, 142);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(83, 42);
+            this.button8.Size = new System.Drawing.Size(111, 52);
             this.button8.TabIndex = 8;
             this.button8.Text = "7";
             this.button8.UseVisualStyleBackColor = true;
@@ -186,9 +120,10 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(280, 67);
+            this.button9.Location = new System.Drawing.Point(373, 82);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(83, 42);
+            this.button9.Size = new System.Drawing.Size(111, 52);
             this.button9.TabIndex = 7;
             this.button9.Text = "6";
             this.button9.UseVisualStyleBackColor = true;
@@ -196,53 +131,136 @@
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(280, 19);
+            this.button10.Location = new System.Drawing.Point(373, 23);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(83, 42);
+            this.button10.Size = new System.Drawing.Size(111, 52);
             this.button10.TabIndex = 6;
             this.button10.Text = "5";
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
-            // pictureBox2
+            // button5
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(12, 17);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(169, 258);
-            this.pictureBox2.TabIndex = 11;
-            this.pictureBox2.TabStop = false;
+            this.button5.Location = new System.Drawing.Point(255, 260);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 52);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // button11
+            // button4
             // 
-            this.button11.BackColor = System.Drawing.Color.Chartreuse;
-            this.button11.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button11.Location = new System.Drawing.Point(218, 37);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(126, 81);
-            this.button11.TabIndex = 13;
-            this.button11.Text = "LIGA";
-            this.button11.UseVisualStyleBackColor = false;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
+            this.button4.Location = new System.Drawing.Point(255, 201);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 142);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 82);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 23);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 318);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button12);
+            this.tabPage2.Controls.Add(this.button11);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(516, 362);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Motor";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // button12
             // 
             this.button12.BackColor = System.Drawing.Color.Red;
             this.button12.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button12.Location = new System.Drawing.Point(218, 180);
+            this.button12.Location = new System.Drawing.Point(291, 222);
+            this.button12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(126, 81);
+            this.button12.Size = new System.Drawing.Size(168, 100);
             this.button12.TabIndex = 14;
             this.button12.Text = "DESLIGA";
             this.button12.UseVisualStyleBackColor = false;
             this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Chartreuse;
+            this.button11.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(291, 46);
+            this.button11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(168, 100);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "LIGA";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(16, 21);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(225, 318);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(408, 333);
+            this.ClientSize = new System.Drawing.Size(544, 410);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -250,8 +268,8 @@
             this.Text = "Abas de Controle";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
