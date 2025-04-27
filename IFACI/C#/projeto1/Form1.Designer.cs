dfd6ee7adb949251a62dd4acb701ef34ec71404b
cortezinho <gabrielcortez2802@gmail.com>
2025-04-27 19:32:41 -0300

    IFACI

---
+++ IFACI/C#/projeto1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace projeto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.AGaugeRange aGaugeRange1 = new System.Windows.Forms.AGaugeRange();
+            System.Windows.Forms.AGaugeRange aGaugeRange2 = new System.Windows.Forms.AGaugeRange();
+            System.Windows.Forms.AGaugeRange aGaugeRange3 = new System.Windows.Forms.AGaugeRange();
+            System.Windows.Forms.AGaugeRange aGaugeRange4 = new System.Windows.Forms.AGaugeRange();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.thermControl1 = new ThermometerControl.ThermControl();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.aGauge1 = new System.Windows.Forms.AGauge();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1064, 556);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1056, 527);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "CONEXÃO";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 39);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 238);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 165);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(40, 63);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(288, 63);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(183, 47);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(1056, 527);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "LED";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(600, 140);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(373, 293);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONTROLE DE LED";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 224);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 71);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "LIGAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.aGauge1);
+            this.tabPage3.Controls.Add(this.thermControl1);
+            this.tabPage3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1056, 527);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "SENSORES";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // thermControl1
+            // 
+            this.thermControl1.Location = new System.Drawing.Point(95, 69);
+            this.thermControl1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.thermControl1.MinimumSize = new System.Drawing.Size(280, 388);
+            this.thermControl1.Name = "thermControl1";
+            this.thermControl1.Size = new System.Drawing.Size(280, 388);
+            this.thermControl1.TabIndex = 0;
+            this.thermControl1.Value = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // aGauge1
+            // 
+            this.aGauge1.BaseArcColor = System.Drawing.Color.Gray;
+            this.aGauge1.BaseArcRadius = 80;
+            this.aGauge1.BaseArcStart = 135;
+            this.aGauge1.BaseArcSweep = 270;
+            this.aGauge1.BaseArcWidth = 2;
+            this.aGauge1.Center = new System.Drawing.Point(100, 100);
+            aGaugeRange1.Color = System.Drawing.Color.Lime;
+            aGaugeRange1.EndValue = 25F;
+            aGaugeRange1.InnerRadius = 50;
+            aGaugeRange1.InRange = false;
+            aGaugeRange1.Name = "baixa";
+            aGaugeRange1.OuterRadius = 80;
+            aGaugeRange1.StartValue = 0F;
+            aGaugeRange2.Color = System.Drawing.Color.Yellow;
+            aGaugeRange2.EndValue = 50F;
+            aGaugeRange2.InnerRadius = 50;
+            aGaugeRange2.InRange = false;
+            aGaugeRange2.Name = "media";
+            aGaugeRange2.OuterRadius = 80;
+            aGaugeRange2.StartValue = 25F;
+            aGaugeRange3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            aGaugeRange3.EndValue = 75F;
+            aGaugeRange3.InnerRadius = 50;
+            aGaugeRange3.InRange = false;
+            aGaugeRange3.Name = "alta";
+            aGaugeRange3.OuterRadius = 80;
+            aGaugeRange3.StartValue = 50F;
+            aGaugeRange4.Color = System.Drawing.Color.Red;
+            aGaugeRange4.EndValue = 100F;
+            aGaugeRange4.InnerRadius = 50;
+            aGaugeRange4.InRange = false;
+            aGaugeRange4.Name = "critica";
+            aGaugeRange4.OuterRadius = 80;
+            aGaugeRange4.StartValue = 75F;
+            this.aGauge1.GaugeRanges.Add(aGaugeRange1);
+            this.aGauge1.GaugeRanges.Add(aGaugeRange2);
+            this.aGauge1.GaugeRanges.Add(aGaugeRange3);
+            this.aGauge1.GaugeRanges.Add(aGaugeRange4);
+            this.aGauge1.Location = new System.Drawing.Point(556, 121);
+            this.aGauge1.MaxValue = 100F;
+            this.aGauge1.MinValue = 0F;
+            this.aGauge1.Name = "aGauge1";
+            this.aGauge1.NeedleColor1 = System.Windows.Forms.AGaugeNeedleColor.Gray;
+            this.aGauge1.NeedleColor2 = System.Drawing.Color.DimGray;
+            this.aGauge1.NeedleRadius = 80;
+            this.aGauge1.NeedleType = System.Windows.Forms.NeedleType.Advance;
+            this.aGauge1.NeedleWidth = 2;
+            this.aGauge1.ScaleLinesInterColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesInterInnerRadius = 73;
+            this.aGauge1.ScaleLinesInterOuterRadius = 80;
+            this.aGauge1.ScaleLinesInterWidth = 1;
+            this.aGauge1.ScaleLinesMajorColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesMajorInnerRadius = 70;
+            this.aGauge1.ScaleLinesMajorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMajorStepValue = 10F;
+            this.aGauge1.ScaleLinesMajorWidth = 2;
+            this.aGauge1.ScaleLinesMinorColor = System.Drawing.Color.Gray;
+            this.aGauge1.ScaleLinesMinorInnerRadius = 75;
+            this.aGauge1.ScaleLinesMinorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMinorTicks = 9;
+            this.aGauge1.ScaleLinesMinorWidth = 1;
+            this.aGauge1.ScaleNumbersColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleNumbersFormat = null;
+            this.aGauge1.ScaleNumbersRadius = 95;
+            this.aGauge1.ScaleNumbersRotation = 0;
+            this.aGauge1.ScaleNumbersStartScaleLine = 0;
+            this.aGauge1.ScaleNumbersStepScaleLines = 1;
+            this.aGauge1.Size = new System.Drawing.Size(310, 282);
+            this.aGauge1.TabIndex = 1;
+            this.aGauge1.Text = "aGauge1";
+            this.aGauge1.Value = 0F;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private ThermometerControl.ThermControl thermControl1;
+        private System.Windows.Forms.AGauge aGauge1;
+    }
+}
+
---
+++ IFACI/C#/projeto1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace projeto1
+{
+    public partial class Form1 : Form
+    {
+        private bool estadoLED = false;
+        private string bufferRecebido = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("c:\\imagens\\Vermelho.bmp");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox2.Image = Image.FromFile("c:\\imagens\\Vermelho.bmp");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            serialPort1.DataReceived += serialPort1_DataReceived;
+
+            label1.Text = "Desconectado";
+        }
+
+        private void atualizaListaCOMs()
+        {
+            string[] portas = SerialPort.GetPortNames();
+
+            if (!portas.SequenceEqual(comboBox1.Items.Cast<string>()))
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(portas);
+                if (comboBox1.Items.Count > 0)
+                    comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort1.Open();
+
+                    button1.Text = "Desconectar";
+                    comboBox1.Enabled = false;
+                    pictureBox1.Image = Image.FromFile("c:\\imagens\\Verde.bmp");
+                    label1.Text = "Conectado";
+                }
+                catch
+                {
+                    MessageBox.Show("Erro ao conectar.");
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.DataReceived -= serialPort1_DataReceived;
+                    serialPort1.Close();
+
+                    button1.Text = "Conectar";
+                    comboBox1.Enabled = true;
+                    pictureBox1.Image = Image.FromFile("c:\\imagens\\Vermelho.bmp");
+                    label1.Text = "Desconectado";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao desconectar: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                estadoLED = !estadoLED;
+
+                serialPort1.WriteLine(estadoLED ? "L" : "D");
+
+                pictureBox2.Image = Image.FromFile(estadoLED ? "c:\\imagens\\Verde.bmp" : "c:\\imagens\\Vermelho.bmp");
+                button2.Text = estadoLED ? "Desligar" : "Ligar";
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!serialPort1.IsOpen) return;
+
+            try
+            {
+                string dados = serialPort1.ReadExisting();
+                bufferRecebido += dados;
+
+                if (bufferRecebido.Contains("\n"))
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        string[] linhas = bufferRecebido.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (linhas.Length > 0)
+                        {
+                            string ultimaLinha = linhas[linhas.Length - 1];
+                            bufferRecebido = ""; // limpar buffer após leitura
+
+                            string[] partes = ultimaLinha.Split(',');
+
+                            if (partes.Length == 2 &&
+                                double.TryParse(partes[0], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double temperatura) &&
+                                double.TryParse(partes[1], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double tensao))
+                            {
+                                thermControl1.UpdateControl((int)temperatura);
+                                aGauge1.Value = (int)(tensao * 20); 
+                            }
+                        }
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Erro na leitura da porta serial: " + ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
